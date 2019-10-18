26e58e4757684a77ca7cf3b314dd710d985c1072
Moe Sy <moe@msy.ai>
2019-10-18 13:21:34 +0000

    Add Celsius-Farenheit table
    Exercise 1-4
    
    Write a program to print a celsius to farenheit table.

--- chapter1/utils.c
+++ chapter1/utils.c
@@ -2,6 +2,41 @@
 
 #define FAHR_MAX 400
 #define FAHR_MIN -459.67
+#define CEL_MAX 150
+#define CEL_MIN -273.15
+
+/*
+ * cel_table - print Celsius-Farenheit table
+ *
+ * @lower            lower limit of the temperature table (floor is 0)
+ * @upper            upper limit (ceiling is CEL_MAX)
+ * @step             step size
+ *
+ * Print a fahr-celsius table bound by the input parameters.
+ *
+ */
+void cel_table(int lower, int upper, int step) {
+
+  float fahr, celsius;
+
+  if (upper >= CEL_MAX)
+    upper = CEL_MAX;
+
+  if (lower < CEL_MIN)
+    lower = CEL_MIN;
+
+  celsius = lower;
+
+  // Print table heading
+  printf("Celsius \t Farenheit\n");
+
+  while (celsius <= upper) {
+    fahr = (celsius * 9.0/5.0) + 32;
+    printf("%3.0f \t\t %6.1f\n", celsius, fahr);
+    celsius += step;
+  }
+
+}
 
 /*
  * fahr_table - print Farenheit-Celsius table
